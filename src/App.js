7bb86a0f01c841d892af7e5058a9f5800672903e
Destwood <DestwoodGames@Gmail.com>
2023-08-23 19:56:36 +0300

    cursor + TicTacToe directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.scss";
+import box from "./assets/svg.svg";
+
+import Cursor from "./components/cursor/Cursor";
 
 function App() {
-  return <div className="App">dsa</div>;
+  return (
+    <div className="App">
+      <Cursor />
+      <img className="img" src={box} alt=""></img>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function TicTacToe() {
+  return (
+    <div className="">
+      <div className=""></div>
+    </div>
+  );
+}
+
+export default TicTacToe;
