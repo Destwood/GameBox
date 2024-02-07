5da43b8422c9c0c2ace66e0774c53eb355ff8cf8
Destwood <DestwoodGames@Gmail.com>
2024-02-07 02:16:43 +0200

    snake game update

--- src/components/Snake/Snake.jsx
+++ src/components/Snake/Snake.jsx
@@ -4,7 +4,7 @@ import style from "./Snake.module.scss";
 function Snake() {
   const [score, setScore] = useState(0);
   const [food, setFood] = useState({ x: 0, y: 0 });
-  const [snake, setSnake] = useState({ x: 1, y: 1 });
+  const [snake, setSnake] = useState({ x: 10, y: 10 });
   const [direction, setDirection] = useState("ArrowRight");
   const [snakeBody, setSnakeBody] = useState([snake]);
 
@@ -12,32 +12,42 @@ function Snake() {
 
   const updateFoodPosition = () => {
     const newFoodPosition = {
-      x: Math.floor(Math.random() * 30) + 1,
-      y: Math.floor(Math.random() * 30) + 1,
+      x: Math.floor(Math.random() * 15) + 1,
+      y: Math.floor(Math.random() * 15) + 1,
     };
     setFood(newFoodPosition);
   };
+
   const updateScores = () => {
+    console.log("updateScore");
+    setScore(score + 1);
     score > highScore
       ? localStorage.setItem("GameBox_Snake_HighScore", score)
       : setScore(score + 1);
   };
 
-  const changeDirection = (dir) => {
-    setDirection(dir);
-  };
-
-  const handleColision = () => {
-    if (snake.x <= 0 || snake.x > 30 || snake.y <= 0 || snake.y > 30) {
-      setScore(0);
-      setSnake({ x: 5, y: 5 });
+  const changeDirection = (newDirection) => {
+    if (
+      (direction === "ArrowLeft" && newDirection !== "ArrowRight") ||
+      (direction === "ArrowRight" && newDirection !== "ArrowLeft") ||
+      (direction === "ArrowUp" && newDirection !== "ArrowDown") ||
+      (direction === "ArrowDown" && newDirection !== "ArrowUp")
+    ) {
+      setDirection(newDirection);
     }
   };
 
+  const handleColision = () => {
+    console.log(snake.x <= 0 || snake.x > 30 || snake.y <= 0 || snake.y > 30);
+  };
+
+  // game updating func
   const updateGame = () => {
+    let newSnakeBody = [...snakeBody];
     if (snake.x === food.x && snake.y === food.y) {
       updateFoodPosition();
-      snakeBody.push([food.x, food.y]);
+      newSnakeBody = [...newSnakeBody, { x: food.x, y: food.y }];
+
       updateScores();
     }
     switch (direction) {
@@ -56,25 +66,40 @@ function Snake() {
       default:
         break;
     }
+    for (let i = newSnakeBody.length - 1; i > 0; i--) {
+      newSnakeBody[i] = snakeBody[i - 1];
+    }
+    newSnakeBody[0] = snake;
 
-    handleColision();
+    if (snake.x <= 0 || snake.x > 30 || snake.y <= 0 || snake.y > 30) {
+      setScore(0);
+      setSnake({ x: 5, y: 5 });
+      updateFoodPosition();
+      setSnakeBody([snake]);
+    } else {
+      setSnakeBody(newSnakeBody);
+    }
   };
+
+  // first render food position update
   useEffect(() => {
     updateFoodPosition();
   }, []);
+
+  // update game each .2 second
   useEffect(() => {
     const handleKeyPress = (e) => {
       changeDirection(e.key);
     };
-    document.addEventListener("keyup", handleKeyPress);
+    document.addEventListener("keydown", handleKeyPress);
 
-    const updateIntervalId = setInterval(updateGame, 500);
+    const updateIntervalId = setInterval(updateGame, 200);
 
     return () => {
       clearInterval(updateIntervalId);
       document.removeEventListener("keydown", handleKeyPress);
     };
-  }, [snakeBody, direction]);
+  }, [snake, snakeBody, score]);
   return (
     <div className={style.container}>
       <div className={style.details}>
@@ -85,46 +110,59 @@ function Snake() {
       <div className={style.board}>
         <div
           className={style.food}
-          style={{ gridArea: `${food.x} / ${food.y}` }}
+          style={{ gridArea: `${food.y} / ${food.x}` }}
         ></div>
-
-        {snakeBody.map((element) => {
-          <div
-            class={style.head}
-            style={{ gridArea: `${snake.y} / ${snake.x}` }}
-          ></div>;
-        })}
+        {snakeBody.map(
+          (item, index) => (
+            console.log(snakeBody),
+            (
+              <div
+                key={index}
+                className={style.head}
+                style={{ gridArea: `${item.y} / ${item.x}` }}
+              ></div>
+            )
+          )
+        )}
       </div>
 
       <div className={style.control}>
-        <button
-          onClick={() => {
-            changeDirection("ArrowLeft");
-          }}
-        >
-          ←
-        </button>
-        <button
-          onClick={() => {
-            changeDirection("ArrowUp");
-          }}
-        >
-          ↑
-        </button>
-        <button
-          onClick={() => {
-            changeDirection("ArrowRight");
-          }}
-        >
-          →
-        </button>
-        <button
-          onClick={() => {
-            changeDirection("ArrowDown");
-          }}
-        >
-          ↓
-        </button>
+        <div className="">
+          <button
+            className={style.sideButton}
+            onClick={() => {
+              changeDirection("ArrowLeft");
+            }}
+          >
+            ←
+          </button>
+        </div>
+        <div className="">
+          <button
+            onClick={() => {
+              changeDirection("ArrowUp");
+            }}
+          >
+            ↑
+          </button>
+          <button
+            onClick={() => {
+              changeDirection("ArrowDown");
+            }}
+          >
+            ↓
+          </button>
+        </div>
+        <div className="">
+          <button
+            className={style.sideButton}
+            onClick={() => {
+              changeDirection("ArrowRight");
+            }}
+          >
+            →
+          </button>
+        </div>
       </div>
     </div>
   );
